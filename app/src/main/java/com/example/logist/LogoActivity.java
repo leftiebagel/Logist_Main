dc58808b51e29671bf8e55007fb4309f205674eb
leftiebagel <jacobwr3@illinois.edu>
2020-05-30 22:06:30 -0500

    Commit 2 electric boogalo

---
+++ app/src/main/java/com/example/logist/LogoActivity.java
@@ -0,0 +1,22 @@
+package com.example.logist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logo_screen);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, OverviewActivity.class);
+            startActivity(intent, savedInstanceState);
+        });
+    }
+}
---
+++ app/src/main/java/com/example/logist/OverviewActivity.java
@@ -0,0 +1,30 @@
+package com.example.logist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class OverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inventory_overview);
+
+        Button newEntry = findViewById(R.id.new_entry);
+        newEntry.setOnClickListener(v -> {
+            //go to dropdown of existing recipes, or give option to build a new one
+        });
+
+        Button recLib = findViewById(R.id.rec_list);
+        recLib.setOnClickListener(v -> {
+            //go to recipe library to edit or view
+        });
+
+        Button backButton = findViewById(R.id.over_back);
+        backButton.setOnClickListener(v -> {
+            finish();
+        });
+    }
+}
